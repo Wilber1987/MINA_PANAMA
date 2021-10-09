7b455d07bb285ab78641eb918bbbcea7d45e1772
Wilber Jose Matus Gonzalez <wilberj1987@gmail.com>
2021-10-09 17:12:28 -0600

    PC HOME

--- Views/TestView.js
+++ Views/TestView.js
@@ -4,7 +4,11 @@ const DOMManager = new ComponentsManager();
 class WTestConfig {
     Title = "Title";
     Descripcion = "descripcion....";
-    Questions = [{ Id: 1, Descripcion: "desc 1", Value: null, Resps: ["SI", "NO", "N/A"] }];
+    YEAR = "";
+    MES = "";
+    COMPONENTE = "";
+    INDICADOR = "";
+    Questions = [{ CRITERIO: "CRITERIO", EXPEDIENTE: "", Value: "SI" }];
 }
 class WTestView extends HTMLElement {
     constructor(Config = (new WTestConfig())) {
@@ -12,6 +16,7 @@ class WTestView extends HTMLElement {
         this.attachShadow({
             mode: 'open'
         });
+        this.GoogleForm = "https://docs.google.com/forms/d/e/1FAIpQLSf1XMp3hGlTl69mvPJbPpK0hdyxzu9i43l20zZdVvTJJaVU4A/viewform";
         this.Config = Config;
         this.MainTestContainer = WRender.createElement({
             type: "div", props: { class: "MainTestContainer" }
@@ -20,24 +25,25 @@ class WTestView extends HTMLElement {
         this.MainTestContainer.append(WRender.CreateStringNode(`<p>${this.Config.Descripcion}</p>`));
         this.MainTestContainer.append(this.DrawTest(this.Config.Questions));
         this.shadowRoot.append(this.MainTestContainer);
+        this.Resps = ["SI", "NO", "N/A"];
     }
     connectedCallback() {
         this.DrawComponent();
     }
     DrawComponent = () => { }
-    DrawTest = (Questions) => {
+    DrawTest = (Questions = this.Config.Questions) => {
         const ContainerQuestions = WRender.createElement({
             type: "div", props: { class: "ContainerQuestions" }
         });
         Questions.forEach(element => {
             const RespContainer = WRender.createElement({ type: "div", props: { class: "RespContainer" } });
-            element.Resps.forEach(Resp => {
+            this.Resps.forEach(Resp => {
                 RespContainer.append(WRender.createElement([
                     { type: 'label', props: { innerText: Resp, htmlFor: "Control" + element.Id + Resp } },
                     {
                         type: 'input', props: {
                             type: 'radio', id: "Control" + element.Id + Resp, name: "Resp" + element.Id,
-                            value: Resp, onchange: (ev)=>{
+                            value: Resp, onchange: (ev) => {
                                 element.Value = ev.target.value;
                             }
                         }
@@ -46,12 +52,40 @@ class WTestView extends HTMLElement {
             });
             const QuestionControl = WRender.createElement({
                 type: "div", props: { class: "QuestionControl" }, children: [
-                    element.Descripcion, RespContainer
+                    element.CRITERIO, RespContainer
                 ]
             });
             ContainerQuestions.append(QuestionControl);
         });
         return ContainerQuestions;
+    }
+    SaveGoogleForm = async (ElementForm = (new WTestConfig()).Questions[0]) => {
+        const url = this.GoogleForm;
+        const data = {
+            "entry.1446800977": this.Config.YEAR,
+            "entry.53598846": this.Config.MES,
+            "entry.1335627763": this.Config.COMPONENTE,
+            "entry.570676050": this.Config.INDICADOR,
+            "entry.1474905730": ElementForm.CRITERIO,
+            "entry.1672193964": ElementForm.EXPEDIENTE,
+            "entry.609427820": ElementForm.Value,
+        }
+        $.ajax({
+            url: url,
+            data: data,
+            type: "POST",
+            dataType: "xml",
+            statusCode: {
+                0: function () {
+                    alert("success 0")
+                    location.reload();
+                },
+                200: function () {
+                    alert("success 200")
+                    //     window.location.replace("Success.html");
+                }
+            }
+        });
     }
 }
 customElements.define('w-test', WTestView);
